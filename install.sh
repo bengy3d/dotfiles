05a0b55fedafc4ae590abb1e99d452e596f5099b
Beniamin Pel <bengy3d@gmail.com>
2025-03-23 00:56:54 +0100

    Added an install script and tmux config

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if ! command -v brew &>/dev/null; then
+    echo "Please install homebrew"
+    exit 1
+fi
+
+brew install nvim htop tmux fzf ripgrep
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+mkdir -p $HOME/.config
+
+ln -s $SCRIPT_DIR/.config/nvim $HOME/.config/nvim
+
+mkdir -p $HOME/.local/bin
+
+ln -s $SCRIPT_DIR/.local/bin/tmux-sessionizer $HOME/.config/nvim
+
+mkdir -p $HOME/Work/Notes
+
